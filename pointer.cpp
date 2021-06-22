5a1e4ead1970456bc9cd1deb2aeef52020a514f8
Amenable-C <duswo0624@naver.com>
2021-06-22 21:33:25 +0900

    0622

---
+++ pointer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1 = 0;
+    int num2 = 0;
+    int num3 = 0;
+
+    cin >> num1 >> num2 >> num3;
+    int sum = 0;
+    sum = num1 + num2 + num3;
+
+    cout << sum;
+
+}
